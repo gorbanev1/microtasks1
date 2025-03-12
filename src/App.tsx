6cf11cde0a102bd384ddc4c3d37fe3649c4a0a05
Igor Gorbanev <gorbanev_i@mail.ru>
2025-03-12 18:55:55 +0300

    filter beginning

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,18 @@ function App() {
     const onClickHandler2=()=>{
         setA(0)
     }
+
+    const [money, setMoney] = useState([
+        {banknote: "dollar", nominal: 100, number: "a123456789"},
+        {banknote: "dollar", nominal: 50, number: "b123456789"},
+        {banknote: "ruble", nominal: 100, number: "c123456789"},
+        {banknote: "dollar", nominal: 100, number: "d123456789"},
+        {banknote: "dollar", nominal: 50, number: "e123456789"},
+        {banknote: "ruble", nominal: 100, number: "f123456789"},
+        {banknote: "dollar", nominal: 50, number: "j123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"}
+    ])
+
     return (<>
             <NewComponent students={students}/>
             <Button callBack={()=>{button1Foo("hui", 2)}} name={"hui"}/>
@@ -49,6 +61,19 @@ function App() {
             <h1>{a}</h1>
             <Button callBack={onClickHandler} name={"adssdas"}>adasd</Button>
             <Button callBack={onClickHandler2} name={"adssdas"}>0</Button>
+
+            <ul>
+                {money.map((objFromMoneyArr) => {
+                    return(
+                        <li>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.nominal}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
         </>
     );
 }
